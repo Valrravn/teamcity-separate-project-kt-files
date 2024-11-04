a8b1f2b3ab35ca0eb3b5c99e1f15e11ed2302765
Valravn <Valravn@TeamCity>
2024-11-04 23:34:56 +0100

    TeamCity change in 'Teamcity Separate Project Kt Files / SubProject A / SubprojectA_2' project: general settings of 'SubprojectA2: Config1' build configuration were updated

--- .teamcity/SubProjectA_SubprojectA2/buildTypes/SubProjectA_SubprojectA2_SubprojectA2Config1.kt
+++ .teamcity/SubProjectA_SubprojectA2/buildTypes/SubProjectA_SubprojectA2_SubprojectA2Config1.kt
@@ -3,5 +3,5 @@ package SubProjectA_SubprojectA2.buildTypes
 import jetbrains.buildServer.configs.kotlin.*
 
 object SubProjectA_SubprojectA2_SubprojectA2Config1 : BuildType({
-    name = "SubprojectA2_Config1"
+    name = "SubprojectA2: Config1"
 })
